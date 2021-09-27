f20b9292afdc1e52acefc4ab08ac609ede1024cd
Prathik B Shetty <prathikshetty9b@gmail.com>
2021-09-28 00:16:48 +0530

    Restructure Day 1

---
+++ Day-1/header.h
@@ -0,0 +1,21 @@
+#define PI 3.14
+#define INVALID -1
+#define EVEN 2
+#define ODD 3
+#define TRUE 1
+#define FALSE 0
+
+
+/*--------------------------------------Day 1----------------------------------*/
+
+
+char lower_case(char c);
+int circle_area(float r);
+int simple_interest(float principal, float timePeriod, float rateofInterest);
+int compound_interest(float principal, float timePeriod, float rateofInterest);
+float fahrenheit_to_celsius(float f);
+float celsius_to_fahrenheit(float c);
+int odd_or_even(int num);
+int is_leap_year(int year);
+int power_of_2(int n);
+
